2336c1be3a46c0ca7ad236376ee0aa97bfe3bdaf
Dovi Ichilevici <dovi1499@gmail.com>
2021-11-24 15:08:00 -0500

    dojo fruit store

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
+from datetime import datetime, time
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
 app = Flask(__name__)  
 
 @app.route('/')         
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
+    # var
+    count = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    # time
+    now = datetime.now()
+    date =now.strftime("%c")
+    # prints..
     print(request.form)
-    return render_template("checkout.html")
+    print(date)
+    print ('count:', count)
+    print ('charging', request.form['first_name'], request.form['last_name'], 'for', count, 'fruit(s)')
+    return render_template("checkout.html" , count=count, date=date)
 
 @app.route('/fruits')         
 def fruits():
